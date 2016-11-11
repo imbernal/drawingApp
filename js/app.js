90080e6914dbcf8efbc1867234df849a1544e1ed
Israel Bernal <imbernal9203@gmail.com>
2016-11-10 22:10:39 -0500

    First Commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var color = $('.selected').css("background-color");
+
+$(".controls li").click(function(){
+
+	$(this).siblings().removeClass("selected");
+
+	$(this).addClass("selected");
+
+	color = $(this).css("background-color");
+
+});
+
+$('#revealColorSelect').click(function(){
+	changeColor();
+	$('#colorSelect').toggle();
+});
+
+function changeColor(){
+	var r = $('#red').val();
+	var g = $('#green').val();
+	var b = $('#blue').val();
+
+	$('#newColor').css('background-color', "rgb(" + r +"," + g + ","+ b+ ")");
+
+
+}
+
+$("input[type=range]").change(changeColor); 
+
+$('#addNewColor').click(function(){
+
+	var $newColor = $('<li></li>');
+
+	$newColor.css("background-color" , $('#newColor').css('background-color'));
+
+	$('.controls ul').append($newColor);
+
+});
